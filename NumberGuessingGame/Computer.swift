2fb5ebce0a27e392defc1a9895255b9992a1cfb6
王冊 <rainbowshan33@gmail.com>
2020-11-16 15:52:10 +0800

    隨機答案

--- NumberGuessingGame/Computer.swift
+++ NumberGuessingGame/Computer.swift
@@ -10,6 +10,9 @@ import Foundation
 
 class Computer: Basic {
     var answerNumberSting = ""
+    var guessCount = 0
+    
+    
     func generateAnswerNumberSting(){
         var numbers = ["1","2","3","4","5","6","7","8","9"]
         for _ in 1 ... MAX_NUMBER{
@@ -21,6 +24,17 @@ class Computer: Basic {
             }
         }
     }
+    
+    func userGuess(input:String) -> (A:Int,B:Int)?{
+        let result = checkAB(test: input, answer: answerNumberSting)
+        if result != nil{
+            guessCount += 1
+        }
+        
+        return result
+    }
+    
+    
     
     override init() {
         super.init()
