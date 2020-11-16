c8e2bc09ad2e6de3f039318a02d7176d4deb2588
王冊 <rainbowshan33@gmail.com>
2020-11-16 15:12:45 +0800

    雙層迴圈

--- NumberGuessingGame/Basic.swift
+++ NumberGuessingGame/Basic.swift
@@ -33,9 +33,32 @@ class Basic {
         }
         
         return true
+        
     }
-
+    func checkAB(test:String,answer:String) ->(A:Int,B:Int)?{
+        if isOKNumber(input: test) == false || isOKNumber(input: answer) == false{
+            return nil
+        }
+        var resultA = 0
+        var resultB = 0
+        let testArray = Array(test)
+        let answerArray = Array(answer)
+        for i in 0 ..< MAX_NUMBER{
+            for j in 0 ..< MAX_NUMBER{
+                if testArray[i] == answerArray[j] && i == j{
+                    resultA += 1
+                }else if testArray[i] == answerArray[j]{
+                    resultB += 1
+                }
+            }
+        }
+        return(A:resultA, B:resultB)
+    }
 }
+    
+
+
+
 
 
 
