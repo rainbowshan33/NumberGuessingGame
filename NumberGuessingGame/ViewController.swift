3d82402c0ada5a0ab91c4640ccb57f2f9ef4a774
王冊 <rainbowshan33@gmail.com>
2020-11-16 11:34:51 +0800

    連結

--- NumberGuessingGame/ViewController.swift
+++ NumberGuessingGame/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var geussTextFiele: UITextField!
+    
+    @IBOutlet weak var inputA: UITextField!
+    
+    @IBOutlet weak var inputB: UITextField!
+    
+    @IBOutlet weak var playerLog: UITextView!
+    
+    @IBOutlet weak var computerLog: UITextView!
+    
+    @IBAction func reset(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func guess(_ sender: UIButton) {
+    }
+    @IBAction func reply(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
